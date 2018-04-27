98c1c2117de61913f8292e372e58b752c8777432
Unknown <slythenegger@hotmail.fr>
2018-04-27 09:43:01 +0200

    Merge branch 'sly'

--- app/src/main/java/com/example/stropee2017/lokacar/MainActivity.java
+++ app/src/main/java/com/example/stropee2017/lokacar/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //test commentaire
+        //test 2
     }
 }
